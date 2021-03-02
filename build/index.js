894ab78b98e04bd35089abef89637bddbe4a5841
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-03-02 10:17:13 -0500

    fixed small typo

--- build/index.js
+++ build/index.js
@@ -1,14 +1,16 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
-    res.status(200).send("hi");
+  res.status(200).send("hi");
 });
 app.listen(4000, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port} 🚀 `);
 });
