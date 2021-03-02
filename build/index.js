713a86762f79d3d898708741b24b2277812b3f51
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-03-02 10:56:20 -0500

    created connection, added entities folder

--- build/index.js
+++ build/index.js
@@ -1,16 +1,44 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-const port = process.env.PORT || 4000;
-app.get("/", (req, res) => {
-  res.status(200).send("hi");
+const typeorm_1 = require("typeorm");
+const Student_1 = require("./Entities/Student");
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const app = express_1.default();
+    const port = process.env.PORT || 4000;
+    const connection = yield typeorm_1.createConnection({
+        type: "postgres",
+        username: "postgres",
+        password: "4512",
+        database: "School",
+        logging: true,
+        host: "127.0.0.1",
+        synchronize: true,
+        port: 5432,
+        entities: [Student_1.Student],
+    });
+    if (connection.isConnected) {
+        console.log("Connected to Postgres");
+    }
+    app.get("/", (req, res) => {
+        res.status(200).send("hi");
+    });
+    app.listen(4000, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 });
-app.listen(4000, () => {
-  console.log(`Server is running on port ${port} 🚀 `);
+main().catch((err) => {
+    console.log(err);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
 import express, { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import { Student } from "./Entities/Student";
 
-const app = express();
-const port = process.env.PORT || 4000;
+const main = async (): Promise<void> => {
+  const app = express();
+  const port = process.env.PORT || 4000;
 
-app.get("/", (req: Request, res: Response): void => {
-  res.status(200).send("hi");
-});
+  const connection = await createConnection({
+    type: "postgres",
+    username: "postgres",
+    password: "4512",
+    database: "School",
+    logging: true,
+    host: "127.0.0.1",
+    synchronize: true,
+    port: 5432,
+    entities: [Student],
+  });
 
-app.listen(4000, (): void => {
-  console.log(`Server is running on port ${port}`);
+  if (connection.isConnected) {
+    console.log("Connected to Postgres");
+  }
+
+  app.get("/", (req: Request, res: Response): void => {
+    res.status(200).send("hi");
+  });
+
+  app.listen(4000, (): void => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
 });
