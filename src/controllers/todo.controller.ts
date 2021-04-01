09e9b0ed597ecf65dda91a8de20d400389696999
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-03-31 20:46:57 -0400

    added functionality for completing todo

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -15,3 +15,15 @@ export const todo = async (req: Request, res: Response): Promise<void> => {
 
   res.status(201).json({ message: "To Do added" });
 };
+
+export const complete = async (req: Request, res: Response): Promise<void> => {
+  const id: number = parseInt(req.body?.id);
+  const t = await ToDo.findOne({ id });
+  if (t) {
+    t.isCompleted = true;
+    await t.save();
+    res.status(200).json({ message: "To Do changed to completed" });
+  } else {
+    res.status(404).end();
+  }
+};
--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { todos } from "../controllers/todo.controller";
+import { complete, todo, todos } from "../controllers/todo.controller";
 const router: Router = Router();
 
 router.route("/todos").get(todos);
+router.route("/todo").post(todo);
+router.route("/todo/complete").put(complete);
 
 export default router;
