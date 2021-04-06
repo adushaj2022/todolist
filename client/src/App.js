f87a484a5d2f7afbcb5230d5bc75502841d06172
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2021-04-05 21:08:37 -0400

    added features for front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@chakra-ui/button";
+import { Input } from "@chakra-ui/input";
+import { Flex, Heading, Stack } from "@chakra-ui/layout";
+import { useState } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
 
 function App() {
+  const [todo, setToDo] = useState({});
+  const [todos, setToDos] = useState([{}]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Nav />
+        <Flex justifyContent="center" alignItems="center">
+          <Stack spacing={3}>
+            <Heading as="h2" size="sm" isTruncated mt={3}>
+              Enter a task
+            </Heading>
+            <Input
+              variant="outline"
+              placeholder="Walk my dog.."
+              width="450px"
+              v
+              mt={3}
+            />
+            <Button colorScheme="teal" size="sm" width="55px">
+              Add
+            </Button>
+          </Stack>
+        </Flex>
+      </Wrapper>
     </div>
   );
 }
---
+++ client/src/components/Wrapper.js
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Wrapper = ({ children, variant = "regular" }) => {
+  return (
+    <Box
+      mt={8}
+      mx="auto"
+      maxW={variant === "regular" ? "1000px" : "400px"}
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
+};
+export default Wrapper;
